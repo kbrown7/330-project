2e17db3954fca8a30cf09ec6ef0d5fa4f5870c8d
Kelly <kbrown7@umw.edu>
2012-11-01 21:30:03 -0400

    SQL database test
    works on localhost, need to look at how to transfer to paprika so
    everyone can have access

--- group/src/Interface.java
+++ group/src/Interface.java
@@ -1,4 +1,5 @@
 
+import java.sql.*;
 public class Interface {
 
 	/**
@@ -6,9 +7,27 @@ public class Interface {
 	 * David Snellings
 	 * Brent Steffey
 	 */
-	public static void main(String[] args) {
+	/**public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Interface i = new Interface();
+		i.connectToAndQueryDatabase("Kbrown","    ");
+	}**/
 
+	public void connectToAndQueryDatabase(String username, String password) {
+
+		try{
+	    Connection con = DriverManager.getConnection(
+	                         "jdbc:mysql://localhost:3306/mydb",
+	                         username,
+	                         password);
+	    System.out.println("HERE");
+	    Statement st = con.createStatement();
+	    String table = 
+	    "CREATE TABLE Employee11(Emp_code integer, Emp_name varchar(10))";
+	    st.executeUpdate(table);
+	    System.out.println("Table creation process successfully!");
 	}
-
+		catch(SQLException e){}
+	}
+	
 }
